320f1cae55015f668c856ec5677cf553f8038be8
greenman727 <cornerx3m@gmail.com>
2024-08-13 18:52:25 +0000

    Created python file for api advancesd task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,18 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit.
-
-    :param subreddit: The subreddit to query.
-    :return: Number of subscribers or 0 if invalid subreddit.
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'CustomUserAgent/0.0.1'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
+        if response.status_code == 400:
             data = response.json()
             subscribers = data['data'].get('subscribers', 0)
             return subscribers
